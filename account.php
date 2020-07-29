c2664d66ac2b2c15723a708fc98fef77589c5b80
Herat Zaveri <hz383@njit.edu>
2020-07-29 00:55:54 -0400

    add gitignore

--- account.php
+++ account.php
@@ -1,6 +1,6 @@
 <?php
 
-$hostname = "sql1.njit.edu"     ; 	  // or "sql2.njit.edu"   OR "sql1.njit.edu"
+$hostname = "sql1.njit.edu"     ; // or "sql2.njit.edu"   OR "sql1.njit.edu"
 $username = "hz383" ;
 $project  = "hz383" ;
 $password = "dWOAkXVkm" ;
